92233d6f9797b6520786aa633c794e87cfacebbb
aditya17raj <adityaraj72820@gmail.com>
2024-03-15 15:28:03 +0530

    bug fixes

--- app/api/cron/route.ts
+++ app/api/cron/route.ts
@@ -8,7 +8,7 @@ import { generateEmailBody, sendEmail } from "@/lib/nodemailer";
 
 export const maxDuration = 5; // This function can run for a maximum of 300 seconds
 export const dynamic = "force-dynamic";
-export const revalidate = 0;
+//export const revalidate = 0;
 
 export async function GET(request: Request) {
   try {
